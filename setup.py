a775e99ec15c13b6a73d6f0d7aedd800392240ec
Jonne Kari <jonne.kari@gmail.com>
2017-09-11 19:48:03 +0300

    Setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='monni',
+    version='',
+    packages=['monni', 'monni.ui', 'monni.ui.lists', 'monni.ui.server', 'monni.ui.favorites', 'monni.games',
+              'monni.games.teeworlds', 'monni.games.urbanterror'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
